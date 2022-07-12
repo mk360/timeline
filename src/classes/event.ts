63f9d52c537e43fa8f9fce0a8d9a9370dfcaa1b2
MetaZ9 <32413721+MetaZ9@users.noreply.github.com>
2022-07-12 21:58:39 +0200

    I forgot this one (code cleanup)

--- src/classes/event.ts
+++ src/classes/event.ts
@@ -4,9 +4,9 @@ class Event implements EventStruct {
 	id: number;
 	name: string;
 	desc?: string;
-	occuring_time: number | number[];
+	occuring_time: number;
 
-	constructor(name: string, date: number[], desc?: string) {
+	constructor(name: string, date: number, desc?: string) {
 		//construct id
 		this.name = name;
 		this.occuring_time = date;
